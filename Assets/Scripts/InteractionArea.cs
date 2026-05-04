a998ab7802e9f8eb060ddbb40624568a12a83088
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2026-05-04 11:54:58 -0300

    destroy by tag

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -19,5 +19,10 @@ public class InteractionArea : MonoBehaviour
     void OnCollisionEnter(Collision col)
     {
         Debug.Log(col.gameObject.name);
+        if(col.gameObject.CompareTag("Pickable"))
+        {
+            Destroy(col.gameObject);
+        }
+        
     }
 }
